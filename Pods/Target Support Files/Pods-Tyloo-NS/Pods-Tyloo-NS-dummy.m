7b0ce55397fcc1e083f7df5c4f7400506860f6c3
teymourk <teymourk@oregonstate.edu>
2017-04-27 18:25:49 -0700

    Files Added To Git

---
+++ Pods/Target Support Files/Pods-Tyloo-NS/Pods-Tyloo-NS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tyloo_NS : NSObject
+@end
+@implementation PodsDummy_Pods_Tyloo_NS
+@end
---
+++ Pods/Target Support Files/Pods-Tyloo-NS/Pods-Tyloo-NS-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Tyloo_NSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Tyloo_NSVersionString[];
+
