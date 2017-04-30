c1127011f9e11962020fb5911003b73f61d66291
teymourk <teymourk@oregonstate.edu>
2017-04-30 12:59:45 -0700

    Live Header and Some UI Modification

---
+++ Tyloo-NS/LiveEvents.swift
@@ -0,0 +1,21 @@
+//
+//  LiveEvents.swift
+//  Tyloo-NS
+//
+//  Created by Kiarash Teymoury on 4/30/17.
+//  Copyright © 2017 Kiarash Teymoury. All rights reserved.
+//
+
+import UIKit
+
+class LiveEvents: BaseCell {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
